6a4be9bd7f9e056152572cff153d77fc7e9ebd6f
Ashton Laney <ajl6a@uvawise.edu>
2020-01-29 17:57:28 -0500

    -tested using leaflet instead of openlayers -added svg file for 2nd floor of darden

---
+++ SeminarBuildingMap/wwwroot/js/Darden2nd.js
@@ -0,0 +1,7 @@
+﻿var map = L.map('mapid', {
+    crs: L.CRS.Simple
+});
+
+var bounds = [[0, 0], [1000, 1000]];
+var image = L.imageOverlay('/images/2ndFloorFinal.svg', bounds).addTo(map);
+map.fitBounds(bounds);
