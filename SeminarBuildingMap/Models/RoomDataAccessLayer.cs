fcd9bb0409682a8ab7823d02b89c44ffe1c37d28
Ashton Laney <ajl6a@uvawise.edu>
2020-02-12 18:56:42 -0500

    -Added functionality that allows rooms to be added through

--- SeminarBuildingMap/Models/RoomDataAccessLayer.cs
+++ SeminarBuildingMap/Models/RoomDataAccessLayer.cs
@@ -1,9 +1,6 @@
-﻿using System;
+﻿using Dapper;
 using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Threading.Tasks;
-using Dapper;
 
 namespace SeminarBuildingMap.Models
 {
@@ -19,5 +16,28 @@ namespace SeminarBuildingMap.Models
             }
             return rooms;
         }
+
+        public void InsertDardenRoom(string rmID, string rmCoords, string _connectionString)
+        {
+            rmCoords = rmCoords.Remove(rmCoords.Length - 1);
+            string[] coordsArray = rmCoords.Split(";");
+            if (coordsArray.Length != 4)
+            {
+                return;
+            }
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                var queryParameters = new DynamicParameters();
+                queryParameters.Add("@rmId", rmID);
+                queryParameters.Add("@rmName", "Darden " + rmID);
+                queryParameters.Add("@rmTopLeftPoint", coordsArray[0]);
+                queryParameters.Add("@rmBottomLeftPoint", coordsArray[1]);
+                queryParameters.Add("@rmBottomRightPoint", coordsArray[2]);
+                queryParameters.Add("@rmTopRightPoint", coordsArray[3]);
+                connection.Execute("up_InsertRoom_Darden", queryParameters, commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+
+
     }
 }
