5be135497e4a0e1abd1f15982821be48f0b4c130
Ashton Laney <ajl6a@uvawise.edu>
2020-02-16 18:06:17 -0500

    -scaffolded Identity into project, but isn't fully configured yet

---
+++ SeminarBuildingMap/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SeminarBuildingMap.Areas.Identity.Data;
+using SeminarBuildingMap.Data;
+
+[assembly: HostingStartup(typeof(SeminarBuildingMap.Areas.Identity.IdentityHostingStartup))]
+namespace SeminarBuildingMap.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<SeminarBuildingMapContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("SeminarBuildingMapContextConnection")));
+
+                services.AddDefaultIdentity<SeminarBuildingMapUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<SeminarBuildingMapContext>();
+            });
+        }
+    }
+}
