e4c35fee4dc2ab1ed9af96012e1c764196c828a4
Ashton Laney <ajl6a@uvawise.edu>
2020-02-26 22:00:30 -0500

    -seperate the OwnedRoomDisplay into multiple pages since it had so much functionality

---
+++ SeminarBuildingMap/Areas/Admin/Pages/EditAvailability.cshtml.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
+using SeminarBuildingMap.GenericClasses;
+
+namespace SeminarBuildingMap.Areas.Admin.Pages
+{
+    public class EditAvailabilityModel : PageModel
+    {
+        private readonly IOptions<ConnectionConfig> _connectionConfig;
+
+        readonly Models.RoomDataAccessLayer ObjRoom = new Models.RoomDataAccessLayer();
+
+        readonly Models.RoomScheduleDataAccessLayer ObjSchedule = new Models.RoomScheduleDataAccessLayer();
+
+        [BindProperty]
+        public Models.RoomSchedule lclNewAvailability { get; set; }
+
+        public IQueryable<Models.RoomSchedule> lclSchedule { get; set; }
+
+        public EditAvailabilityModel(IOptions<ConnectionConfig> connectionConfig)
+        {
+            _connectionConfig = connectionConfig;
+        }
+
+        public void OnGet(int id)
+        {
+            lclSchedule = ObjSchedule.GetRoomSchedule(id, _connectionConfig.Value.ConnStr);
+            foreach (Models.RoomSchedule availability in lclSchedule)
+            {
+                availability.convertToEst();
+            }
+        }
+
+        public void OnPost(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                lclNewAvailability.convertToUtc();
+                lclNewAvailability.avRoom = id;
+                ObjSchedule.InsertRoomAvailability(lclNewAvailability, _connectionConfig.Value.ConnStr);
+            }
+        }
+    }
+}
