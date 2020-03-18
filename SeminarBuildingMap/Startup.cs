50ce4f835dfee5877d08ee882a249cb774d71925
annaeden999 <annaeden999@gmail.com>
2020-03-17 22:15:35 -0400

    Changed the navbar color to match the color of the UVa-Wise website navbar. Changed the font of the navbar. Updated heading h1 to be the map title and gave it the id: hMapTitle so that it can be updated to reflect the map that is being shown.

--- SeminarBuildingMap/Startup.cs
+++ SeminarBuildingMap/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SeminarBuildingMap.Areas.Identity.Data;
 using SeminarBuildingMap.GenericClasses;
+using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
 
 namespace SeminarBuildingMap
 {
@@ -29,6 +30,7 @@ namespace SeminarBuildingMap
         {
             services.AddRazorPages();
             services.Configure<ConnectionConfig>(Configuration.GetSection("ConnectionStrings"));
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.Configure<IdentityOptions>(options =>
             {
                 //Password settings
