7bc7dd7c8a8a78b5d9e3a8288cd8ffc455ae1164
Ashton Laney <ajl6a@uvawise.edu>
2020-03-19 23:59:47 -0400

    -add search screen for displaying buildings or adding new building -add search/edit screen to see floors for a building or add new floors to the building

--- SeminarBuildingMap/Areas/Admin/Pages/BuildingEditDisplay.cshtml.cs
+++ SeminarBuildingMap/Areas/Admin/Pages/BuildingEditDisplay.cshtml.cs
@@ -1,39 +1,39 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using SeminarBuildingMap.GenericClasses;
-using SeminarBuildingMap.Models;
 
-namespace SeminarBuildingMap
+namespace SeminarBuildingMap.Areas.Admin.Pages
 {
+    [Authorize(Roles = "Admin")]
     public class BuildingEditDisplayModel : PageModel
     {
+        readonly Models.BuildingDataAccessLayer ObjBuilding = new Models.BuildingDataAccessLayer();
 
-        private readonly IOptions<ConnectionConfig> _connectionConfig;
+        private readonly IOptions<GenericClasses.ConnectionConfig> _connectionConfig;
 
-        readonly RoomDataAccessLayer objRoom = new RoomDataAccessLayer();
+        [BindProperty]
+        public string flNo { get; set; }
 
-        public IEnumerable<Room> Rooms { get; set; }
-
-        public BuildingEditDisplayModel(IOptions<ConnectionConfig> connectionConfig)
+        public IQueryable<Models.Floor> FloorData { get; set; }
+        public BuildingEditDisplayModel(IOptions<GenericClasses.ConnectionConfig> connectionConfig)
         {
             _connectionConfig = connectionConfig;
         }
 
-        public void OnGet()
+        public void OnGet(string bdId)
         {
-            Rooms = objRoom.GetSelectedRooms("Darden", "2", _connectionConfig.Value.ConnStr);
+            FloorData = ObjBuilding.GetBuildingFloors(bdId, _connectionConfig.Value.ConnStr);
         }
 
-        public JsonResult OnGetAdd(String RoomNumber, String Coords)
+        public void OnPost(string bdId)
         {
-            objRoom.InsertDardenRoom(RoomNumber, Coords, _connectionConfig.Value.ConnStr);
-            return new JsonResult("[success]");
+            ObjBuilding.AddFloor(bdId, flNo, _connectionConfig.Value.ConnStr);
+            FloorData = ObjBuilding.GetBuildingFloors(bdId, _connectionConfig.Value.ConnStr);
         }
     }
 }
