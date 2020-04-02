50376292ae9769051ed64f7098892d05ff3cb50f
optimalnaptime <camerondstanley@gmail.com>
2020-04-02 00:25:21 -0400

    -Added Building Grid -Added Room Grid (Only gets Darden currently)

--- SeminarBuildingMap/Pages/BuildingList.cshtml.cs
+++ SeminarBuildingMap/Pages/BuildingList.cshtml.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
 
 namespace SeminarBuildingMap.Pages
 {
     public class BuildingListModel : PageModel
     {
+
+        readonly Models.BuildingDataAccessLayer ObjBuilding = new Models.BuildingDataAccessLayer();
+
+        private readonly IOptions<GenericClasses.ConnectionConfig> _connectionConfig;
+
+        public IQueryable<Models.Building> BuildingData { get; set; }
+
+        public BuildingListModel(IOptions<GenericClasses.ConnectionConfig> connectionConfig)
+        {
+            _connectionConfig = connectionConfig;
+        }
+
         public void OnGet()
         {
+            BuildingData = ObjBuilding.GetAllBuildings(_connectionConfig.Value.ConnStr);
         }
+
     }
 }
--- SeminarBuildingMap/Pages/RoomList.cshtml.cs
+++ SeminarBuildingMap/Pages/RoomList.cshtml.cs
@@ -4,14 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
 
 namespace SeminarBuildingMap
 {
     public class RoomListModel : PageModel
     {
+        readonly Models.RoomDataAccessLayer ObjRoom = new Models.RoomDataAccessLayer();
+
+        private readonly IOptions<GenericClasses.ConnectionConfig> _connectionConfig;
+
+        public IQueryable<Models.Room> BuildingData { get; set; }
+
+        public RoomListModel(IOptions<GenericClasses.ConnectionConfig> connectionConfig)
+        {
+            _connectionConfig = connectionConfig;
+        }
+
         public void OnGet()
         {
-
+            //Needs to be able to change buildings. Currently it shows only dardens rooms.
+            BuildingData = ObjRoom.GetBuildingRooms("DARDEN", _connectionConfig.Value.ConnStr);
         }
     }
 }
